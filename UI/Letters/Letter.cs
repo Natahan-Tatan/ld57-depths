b499b593ed3175a8acbd11698f0b5d6cbb2f14a6
Natahan <me@natahan.net>
2025-04-05 17:37:13 +0200

    💄 Add letter scrolling by keyboard and joystick

--- UI/Letters/Letter.cs
+++ UI/Letters/Letter.cs
@@ -3,10 +3,26 @@ using System;
 
 public class Letter : MarginContainer
 {
+    [Export(PropertyHint.Range,"100,500")]
+    public int ScrollSpeed {get;set;} = 20;
     private ScrollContainer _scrollContainer;
     public override void _Ready()
     {
         _scrollContainer = GetNode<ScrollContainer>("VBoxContainer/ScrollContainer");
     }
 
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+
+        if(Input.IsActionPressed("ui_up", true))
+        {
+            _scrollContainer.ScrollVertical += Mathf.RoundToInt(ScrollSpeed * delta);
+        }
+
+        if(Input.IsActionPressed("ui_down", true))
+        {
+            _scrollContainer.ScrollVertical -= Mathf.RoundToInt(ScrollSpeed * delta);
+        }
+    }
 }
