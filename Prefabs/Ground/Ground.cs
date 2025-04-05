bb0323169f1cc8b00155ad28ec77d88ac4bc9025
Natahan <me@natahan.net>
2025-04-05 09:11:22 +0200

    ✨ Create ground/wall block - Static body - Light occluder

--- Prefabs/Ground/Ground.cs
+++ Prefabs/Ground/Ground.cs
@@ -1,21 +1,27 @@
 using Godot;
 using System;
 
+[Tool]
 public class Ground : StaticBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private Polygon2D _geometry;
+    private CollisionPolygon2D _collisionBox;
+    private OccluderPolygon2D _lightOccluderPolygon;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _geometry = GetNode<Polygon2D>("Polygon2D");
+        _collisionBox = GetNode<CollisionPolygon2D>("CollisionPolygon2D");
+        _lightOccluderPolygon = GetNode<LightOccluder2D>("LightOccluder2D").Occluder;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+
+        #if TOOLS
+            _lightOccluderPolygon.Polygon = _collisionBox.Polygon = _geometry.Polygon;
+        #endif
+    }
+
 }
