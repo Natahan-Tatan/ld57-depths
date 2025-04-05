97fac68753ea0a7b67d62f64fe1c1b8fdc4f335a
Natahan <me@natahan.net>
2025-04-06 00:38:36 +0200

    ✨ Intergrate letter opening system

---
+++ Prefabs/Letter/LetterArea.cs
@@ -0,0 +1,92 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class LetterArea : Area2D
+{
+    public Dictionary<string, bool> HaveBeenOpened = new Dictionary<string, bool>();
+
+    [Signal]
+    public delegate void LetterDiscovered(string identifier, bool isClue);
+
+    [Export]
+    public string Identifier {get;set;}
+
+    [Export]
+    public bool IsClue {get;set;} = false;
+
+    [Export(PropertyHint.MultilineText)]
+    public string Content {
+        get => _content;
+        set {
+            _content = value;
+            GetNode<RichTextLabel>("Letter/VBoxContainer/Scroll/ScrollContainer/MarginContainer/PanelContainer/RichTextLabel").BbcodeText = _content;
+        }
+    }
+    public string _content = "";
+
+    private AnimatedSprite _animatedSprite;
+    private Label _label;
+    private Letter _letterUI;
+
+    private bool _playerIn = false;
+    public override void _Ready()
+    {
+        if(!HaveBeenOpened.ContainsKey(Identifier))
+        {
+            HaveBeenOpened[Identifier] = false;
+        }
+        else if(HaveBeenOpened[Identifier])
+        {
+            _animatedSprite.Play("opened");
+        }
+        else
+        {
+            _animatedSprite.Play("closed");
+        }
+
+        _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        _label = GetNode<Label>("LabelCanvasLayer/NodeLabel/Label");
+        _letterUI = GetNode<Letter>("Letter");
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+
+        if(_playerIn && @event.IsActionPressed("Interact"))
+        {
+            HaveBeenOpened[Identifier] = true;
+            _animatedSprite.Play("opened");
+            GetTree().Paused = true;
+            _letterUI.Visible = true;
+        }
+    }
+
+    public void _on_Letter_Closed()
+    {
+        _letterUI.Visible = false;
+        GetTree().Paused = false;
+        EmitSignal(nameof(LetterDiscovered), Identifier, IsClue);
+    }
+
+    public void _on_Letter_body_entered(Node body)
+    {
+        if(body is Player player)
+        {
+            GD.Print("Appears !");
+            CreateTween().TweenProperty(_label, "modulate", Colors.White, 0.3f);
+            _playerIn = true;
+        }
+    }
+
+    public void _on_Letter_body_exited(Node body)
+    {
+        if(body is Player player)
+        {
+            GD.Print("Disappears !");
+            CreateTween().TweenProperty(_label, "modulate", Colors.Transparent, 0.3f);
+            _playerIn = false;
+        }
+    }
+}
--- UI/Letters/Letter.cs
+++ UI/Letters/Letter.cs
@@ -1,14 +1,41 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class Letter : MarginContainer
 {
+    [Signal]
+    public delegate void Closed();
+
     [Export(PropertyHint.Range,"100,500")]
     public int ScrollSpeed {get;set;} = 20;
+
+    [Export]
+    public new bool Visible {
+        get => base.Visible;
+        set {
+            base.Visible = value;
+            
+            if(_layers != null)
+            {
+                foreach(var l in _layers)
+                {
+                    l.Visible = value;
+                }
+            }
+        }
+    } 
+    
     private ScrollContainer _scrollContainer;
+    private  CanvasLayer[] _layers;
+
     public override void _Ready()
     {
-        _scrollContainer = GetNode<ScrollContainer>("VBoxContainer/ScrollContainer");
+        _scrollContainer = GetNode<ScrollContainer>("VBoxContainer/Scroll/ScrollContainer");
+
+        _layers = GetNode<Control>("VBoxContainer").GetChildren().OfType<CanvasLayer>().ToArray();
+
+        Visible = Visible;
     }
 
     public override void _Process(float delta)
@@ -24,5 +51,10 @@ public class Letter : MarginContainer
         {
             _scrollContainer.ScrollVertical -= Mathf.RoundToInt(ScrollSpeed * delta);
         }
+    }
+
+    public void _on_CloseButton_pressed()
+    {
+        EmitSignal(nameof(Closed));
     }
 }
